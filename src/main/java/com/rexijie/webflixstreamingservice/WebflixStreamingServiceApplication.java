4073b593140743842ce9e91db387deabc6836cf1
Ijiekhuamen Rex <dev.rexijie@gmail.com>
2018-06-09 17:50:29 +0100

    created repo

---
+++ src/main/java/com/rexijie/webflixstreamingservice/WebflixStreamingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.rexijie.webflixstreamingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebflixStreamingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebflixStreamingServiceApplication.class, args);
+    }
+}
