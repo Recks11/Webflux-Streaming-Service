7a467fa2656fb101e9d550060f0eccf7e6250927
Ijiekhuamen Rex <dev.rexijie@gmail.com>
2020-04-05 18:28:18 +0100

    Reformat code

--- src/main/java/com/rexijie/webflixstreamingservice/api/v1/handlers/VideoRouteHandler.java
+++ src/main/java/com/rexijie/webflixstreamingservice/api/v1/handlers/VideoRouteHandler.java
@@ -1,6 +1,5 @@
 package com.rexijie.webflixstreamingservice.api.v1.handlers;
 
-import com.rexijie.webflixstreamingservice.exceptions.VideoNotFoundException;
 import com.rexijie.webflixstreamingservice.services.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.UrlResource;
@@ -33,12 +32,12 @@ public class VideoRouteHandler {
         Mono<ResourceRegion> resourceRegionMono = videoService.getRegion(videoResourceMono, requestHeaders);
 
         return resourceRegionMono.zipWith(videoResourceMono, (resourceRegion, video) -> ServerResponse
-                    .status(HttpStatus.PARTIAL_CONTENT)
-                    .contentType(MediaTypeFactory.getMediaType(video)
-                            .orElse(MediaType.APPLICATION_OCTET_STREAM))
-                    .contentLength(resourceRegion.getCount())
-                    .headers(headers -> headers.setCacheControl(CacheControl.noCache()))
-                    .body(resourceRegionMono, ResourceRegion.class))
+                .status(HttpStatus.PARTIAL_CONTENT)
+                .contentType(MediaTypeFactory.getMediaType(video)
+                        .orElse(MediaType.APPLICATION_OCTET_STREAM))
+                .contentLength(resourceRegion.getCount())
+                .headers(headers -> headers.setCacheControl(CacheControl.noCache()))
+                .body(resourceRegionMono, ResourceRegion.class))
                 .flatMap(serverResponseMono -> serverResponseMono)
                 .doOnError(throwable -> {
                     throw Exceptions.propagate(throwable);
